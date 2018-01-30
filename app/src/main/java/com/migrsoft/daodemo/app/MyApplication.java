067a15aa3096e61a77b0df405e6f68d825adaad7
tangfei <tangf@migrsoft.com>
2018-01-30 20:03:08 +0800

    Initial commit

---
+++ app/src/main/java/com/migrsoft/daodemo/app/MyApplication.java
@@ -0,0 +1,21 @@
+package com.migrsoft.daodemo.app;
+
+import android.app.Application;
+
+/**
+ * Created by dev1 on 2018/1/30.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
